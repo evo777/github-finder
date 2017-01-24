1c50a0d5408f7c55587432b26db233041f4b733e
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-01-23 21:04:05 -0800

    Implemented initial state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,16 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      username: 'evo777',
+      userData: [],
+      userRepos: [],
+      perPage: 10
+    }
+  }
+
   render() {
     return (
       <div>Hi</div>
