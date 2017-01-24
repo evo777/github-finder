3fd8c75d6d1d4e9b4f8a019286ddae4ec4096236
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-01-23 22:40:07 -0800

    Created RepoList component

---
+++ src/components/github/RepoList.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Repo from './Repo.jsx';
+
+class RepoList extends Component {
+
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+
+export default RepoList;
