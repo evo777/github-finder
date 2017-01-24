cdda741276749226f31cdf5d778812b4e348aef0
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-01-23 20:39:05 -0800

    Implemented root component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,12 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Hi</div>
+    )
+  }
+}
+
+export default App;
