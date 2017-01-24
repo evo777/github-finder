8f818922019c2bfeef83945f68b29c5a9469eb7e
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-01-23 20:35:45 -0800

    Implemented ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-alert('Hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementbyId('app'));
