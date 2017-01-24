d84cf965829e557bc0b81ab69e2eb966e9bf27a2
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-01-23 21:12:14 -0800

    Implemented github username display on screen

--- src/components/github/Profile.jsx
+++ src/components/github/Profile.jsx
@@ -1,9 +1,19 @@
 import React, {Component} from 'react';
+import RepoList from './RepoList.jsx'
 
 class Profile extends Component {
-  render() {
 
+  //When using this.props.userData.name, you are getting the name property from the userData
+  render() {
+    return (
+      <div className="panel panel-default">
+        <div className="panel-heading">
+          <h3 className="panel-title">{this.props.userData.name}</h3>
+        </div>
+      </div>
+    )
   }
 }
+
 
 export default Profile;
