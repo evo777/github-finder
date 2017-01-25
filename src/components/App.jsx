23d5baf215f4a8af649f1c5d13813ba4aefb7c7f
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-01-24 21:02:33 -0800

    Imported Profile and Search components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import Profile from './github/Profile.jsx';
+import Search from './github/Search.jsx';
 
 class App extends Component {
   constructor(props) {
